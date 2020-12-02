2e4ae2c11850bc97d820e446c684c337910bc944
Ashish Sarin <ashish.sarin@getfarey.com>
2020-12-02 19:28:10 +0530

    test_branch34

--- dangerfile.js
+++ dangerfile.js
@@ -1,3 +1,3 @@
 import { message, danger, warn, fail, markdown } from "danger";
 const modifiedMD = danger.git.modified_files.join("_ ");
-warn("Changed Files in the PR: \n - " + modifiedMD);
+fail("Changed Files in the PR:123 \n - " + modifiedMD);
